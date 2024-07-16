d948687766c757822f2cebb230b346f4d194a257
Wade <wade.park@mavencloudservice.com>
2024-07-16 09:19:52 +0900

    🐳docker-compose 파일 생성

--- app/main.py
+++ app/main.py
@@ -1,5 +1,9 @@
-from fastapi import FastAPI
+import os
+import sys
 
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
+
+from fastapi import FastAPI
 from utils import lifespan
 from routes.auth import controller as auth_controller
 from routes.page import controller as page_controller
