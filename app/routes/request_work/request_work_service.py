b753c566aff42bcf8dfee57f3ee5289c085bf0bc
Wade <wade.park@mavencloudservice.com>
2024-07-19 15:17:24 +0900

    :sparkles: 작업삭제 PUT API 기능 개발

--- app/routes/request_work/request_work_service.py
+++ app/routes/request_work/request_work_service.py
@@ -25,4 +25,33 @@ async def postRequestWork(
         "delYn":delYn
     }
     workRequestCollection.insert_one(document)
+
+async def updateModifyRequestWork(
+        id: str,
+        userId: str,
+        deviceNm: str,
+        requestTitle: str,
+        customerNm: str,
+        requestDt: str,
+        workContent: str,
+        file: str
+        ):
+    filter = {"_id": id}
+    reqData = objectCleaner.cleanObject({
+        "userId": userId,
+        "deviceNm": deviceNm,
+        "requestTitle": requestTitle,
+        "customerNm": customerNm,
+        "requestDt": requestDt,
+        "workContent": workContent
+        })
+    workRequestCollection.update_one(filter, {"$set": reqData})
+    workRequestCollection.update_one(filter, {"$set":{"file": file}})
+
+async def updateRecoveryRequestWork(
+        id: str
+        ):
+    filter = {"_id": id}
+    workRequestCollection.update_one(filter, {"$set":{"delYn": "Y"}})
+
     return "success"
