8105a1fea9b4dfc1bac599f93968ebe8c835475b
Lucas Lee <lucas.lee@mavencloudservice.com>
2024-07-25 14:24:42 +0900

    :ambulance: SEPARATE 추가

--- app/routes/_path/api_paths.py
+++ app/routes/_path/api_paths.py
@@ -7,33 +7,33 @@ V1 = ROOT + "v1"
 # Auth paths
 AUTH = V1 + SEPARATE + "AUTH"
 LOGIN_WITH_MS = AUTH + SEPARATE + "login"
-AUTH_CALLBACK = AUTH + "auth" + SEPARATE + "callback"
-LOGOUT = AUTH + "logout"
-CHECK_SESSION = AUTH + "validate"
+AUTH_CALLBACK = AUTH + SEPARATE + "auth" + SEPARATE + "callback"
+LOGOUT = AUTH + SEPARATE + "logout"
+CHECK_SESSION = AUTH + SEPARATE + "validate"
 
 # User paths
-USER = V1 + "USER"
+USER = V1 + SEPARATE + "USER"
 READ_USER = USER + SEPARATE + "readUser"
 CREATE_USER = USER + SEPARATE + "createUser"
 UPDATE_USER = USER + SEPARATE + "updateUser"
 DELETE_USER = USER + SEPARATE + "deleteUser"
 
 # Request paths
-REQUEST = V1 + "REQUEST"
+REQUEST = V1 + SEPARATE + "REQUEST"
 READ_REQUEST = REQUEST + SEPARATE + "readRequest"
 CREATE_REQUEST = REQUEST + SEPARATE + "createRequest"
 UPDATE_REQUEST = REQUEST + SEPARATE + "updateRequest"
 DELETE_REQUEST = REQUEST + SEPARATE + "deleteRequest"
 
 # Contract paths
-CONTRACT = V1 + "CONTRACT"
+CONTRACT = V1 + SEPARATE + "CONTRACT"
 READ_CONTRACT = CONTRACT + SEPARATE + "readContract"
 CREATE_CONTRACT = CONTRACT + SEPARATE + "createContract"
 UPDATE_CONTRACT = CONTRACT + SEPARATE + "updateContract"
 DELETE_CONTRACT = CONTRACT + SEPARATE + "deleteContract"
 
 # Tax paths
-TAX = V1 + "TAX"
+TAX = V1 + SEPARATE + "TAX"
 READ_TAX = TAX + SEPARATE + "readTax"
 CREATE_TAX = TAX + SEPARATE + "createTax"
 UPDATE_TAX = TAX + SEPARATE + "updateTax"
