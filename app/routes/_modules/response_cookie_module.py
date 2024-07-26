683ff1e3598518a25db1aa36a9b8921c0ec91d3a
Wade <wade.park@mavencloudservice.com>
2024-07-26 11:57:51 +0900

    :bug: response_cookie_module 잘못된 변수 import 수정

--- app/routes/_modules/response_cookie_module.py
+++ app/routes/_modules/response_cookie_module.py
@@ -1,23 +1,14 @@
 import json
 import pymongo
 
-from db.context import work_request_collection, auth_collection, user_collection
-from fastapi import Request, Response
+from db.context import auth_collection
 from fastapi.responses import JSONResponse
-from db.context import Database
-from routes.auth import auth_service
-from datetime import datetime
-from bson import ObjectId
-from utils import objectCleaner
-from models.work_request_dto import WorkRequestModel, UpdateWorkRequestModel
 from constants import COOKIES_KEY_NAME
-from utils.objectId_convert import objectId_convert
-from routes._modules import list_module
-from routes._modules.list_module import NotNull
 
-def set_response_cookie(token_data: dict, content: any, access_token: str):
-        
+async def set_response_cookie(token_data: dict, content: json):
         response = JSONResponse(content=content)
-        response.set_cookie(key=COOKIES_KEY_NAME, value=access_token, httponly=True)
-
+        if token_data['status'] == "refresh":
+            access_token = auth_collection.find_one({"user_id": token_data['userId']})['access_token']
+            response.set_cookie(key=COOKIES_KEY_NAME, value=access_token, httponly=True)
+            
         return response
